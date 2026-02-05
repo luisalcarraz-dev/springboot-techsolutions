aeef73c0a47eeb6c2eaf81c3de216326631d0772
Luis Alcarraz <lalcarrazneyra@gmail.com>
2026-02-05 18:23:54 -0500

    Primer commit

---
+++ src/main/java/com/TechSolutions/Soporte/TechSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.TechSolutions.Soporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionsApplication.class, args);
+	}
+
+}
