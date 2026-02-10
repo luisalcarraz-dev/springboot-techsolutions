ca432825d887c2b128e41787a05aea756f509095
Luis Alcarraz <lalcarrazneyra@gmail.com>
2026-02-09 22:32:33 -0500

    UsuarioRepository

--- src/main/java/com/TechSolutions/Soporte/Repository/UsuarioRepository.java
+++ src/main/java/com/TechSolutions/Soporte/Repository/UsuarioRepository.java
@@ -1,7 +1,13 @@
 package com.TechSolutions.Soporte.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.TechSolutions.Soporte.model.Usuario;
 
 public interface UsuarioRepository extends  JpaRepository<UsuarioRepository, Integer>{
 
+	Optional<Usuario> findByUsername(String username);
+	
 }
