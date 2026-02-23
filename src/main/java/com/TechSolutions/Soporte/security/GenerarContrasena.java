16aff09a71b5ea293c7baf5b63a1be24eb59b69a
Luis Alcarraz <lalcarrazneyra@gmail.com>
2026-02-23 16:51:41 -0500

    credenciales y diseños pequeños

--- src/main/java/com/TechSolutions/Soporte/security/GenerarContrasena.java
+++ src/main/java/com/TechSolutions/Soporte/security/GenerarContrasena.java
@@ -10,16 +10,20 @@ public class GenerarContrasena {
 
         String adminHash   = encoder.encode("admin1234");
         String tecnicoHash = encoder.encode("soporte1234");
-        String tecnico1Hash = encoder.encode("tecnico3");
-        String tecnico2Hash = encoder.encode("tecnico4");
+        String tecnico1Hash = encoder.encode("2soporte1234");
+        String tecnico2Hash = encoder.encode("3soporte1234");
         String usuarioHash = encoder.encode("Maria1234");
+        String usuario1Hash = encoder.encode("Kevin1234");
+        String usuario2Hash = encoder.encode("Angel1234");
         String jefeHash = encoder.encode("jefe1234");
 
         System.out.println("ADMIN:   " + adminHash);
         System.out.println("TECNICO: " + tecnicoHash);
         System.out.println("TECNICO3: " + tecnico1Hash);
         System.out.println("TECNICO4: " + tecnico2Hash);
-        System.out.println("USUARIO: " + usuarioHash);      
+        System.out.println("USUARIO: " + usuarioHash); 
+        System.out.println("USUARIO1: " + usuario1Hash); 
+        System.out.println("USUARIO2: " + usuario2Hash); 
         System.out.println("JEFE: " + jefeHash);
     }
 }
